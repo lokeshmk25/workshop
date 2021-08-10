9916211d9767f86434c3d78e209c6a22fb6daa19
lokeshmk25 <lokeshmk2211@gmail.com>
2021-08-10 09:46:01 +0530

    Added: new board

--- src/com/bridgelabz/TicTacToeGame.java
+++ src/com/bridgelabz/TicTacToeGame.java
@@ -2,10 +2,25 @@ package com.bridgelabz;
 
 
 public class TicTacToeGame {
+
+    public static char[] newboard() {
+        //variables
+        char[] board = new char[10];
+
+        //loop iterates for 9 times
+
+        for (int index = 1; index <board.length; index++) {
+            //assingn empty spaces to character
+            board[index] = ' ';
+        }
+        //returning the board
+        return board;
+    }
     public static void main(String[] args) {
-        System.out.println("welcome to ticTacToe");
+        /* calling the function from mainmethod */
+        System.out.println("Welcome to tictactoe");
+        newboard();
 
     }
-
 }
 
