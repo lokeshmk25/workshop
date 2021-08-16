34cdf0391514aeccb33d8831f4afdef3e8fef743
lokeshmk25 <lokeshmk2211@gmail.com>
2021-08-16 19:54:29 +0530

    Added:show board

--- src/com/bridgelabz/TicTacToeGame.java
+++ src/com/bridgelabz/TicTacToeGame.java
@@ -5,33 +5,46 @@ import java.util.Scanner;
 
 public class TicTacToeGame {
 
+    private static int[][] board;
+
     public static char[] ticTacToeBoard() {
         //variables
         char[] board = new char[10];
 
         //loop iterates 9 times
 
-        for (int index = 1; index <board.length; index++) {
+        for (int index = 1; index < board.length; index++) {
             //assingn empty spaces to character
-            board[index] = ' ';
+            board[index] = '_';
         }
         //returning the board
         return board;
     }
+
     static void players() {
         char computer;
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the option 'X' or 'O' :" );
+        System.out.println("Enter the option 'X' or 'O' :");
         char player1 = scanner.next().charAt(0);
 
-        if(player1 == 'X' ){
+        if (player1 == 'X') {
             computer = 'O';
-        }else{
-            player1 ='O';
+        } else {
+            player1 = 'O';
             computer = 'X';
         }
-        System.out.println("player option is:" +player1);
-        System.out.println("computer is:" +computer);
+        System.out.println("player option is:" + player1);
+        System.out.println("computer is:" + computer);
+    }
+
+
+     static void showBoard() {
+        board= new int[3][3];
+        System.out.println(board[0][0] + "|" + board[0][1] + "|" + board[0][2]);
+        System.out.println("-+-+-");
+        System.out.println(board[1][0] + "|" + board[1][1] + "|" + board[1][2]);
+        System.out.println("-+-+-");
+        System.out.println(board[2][0] + "|" + board[2][1] + "|" + board[2][2]);
     }
 
 
@@ -40,7 +53,7 @@ public class TicTacToeGame {
         System.out.println("Welcome to tictactoe");
         ticTacToeBoard();
         players();
+        showBoard();
 
     }
 }
-
