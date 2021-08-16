5b9021a0b523bf0dbd4fa4d02bdad31e71de664d
lokeshmk25 <lokeshmk2211@gmail.com>
2021-08-16 12:33:59 +0530

    Added:players option

--- src/com/bridgelabz/TicTacToeGame.java
+++ src/com/bridgelabz/TicTacToeGame.java
@@ -1,13 +1,15 @@
 package com.bridgelabz;
 
 
+import java.util.Scanner;
+
 public class TicTacToeGame {
 
-    public static char[] newboard() {
+    public static char[] ticTacToeBoard() {
         //variables
         char[] board = new char[10];
 
-        //loop iterates for 9 times
+        //loop iterates 9 times
 
         for (int index = 1; index <board.length; index++) {
             //assingn empty spaces to character
@@ -16,10 +18,28 @@ public class TicTacToeGame {
         //returning the board
         return board;
     }
+    static void players() {
+        char computer;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the option 'X' or 'O' :" );
+        char player1 = scanner.next().charAt(0);
+
+        if(player1 == 'X' ){
+            computer = 'O';
+        }else{
+            player1 ='O';
+            computer = 'X';
+        }
+        System.out.println("player option is:" +player1);
+        System.out.println("computer is:" +computer);
+    }
+
+
     public static void main(String[] args) {
         /* calling the function from mainmethod */
         System.out.println("Welcome to tictactoe");
-        newboard();
+        ticTacToeBoard();
+        players();
 
     }
 }
